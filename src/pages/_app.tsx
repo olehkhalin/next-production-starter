e8caea019f2aa0fd042bc98dc0fa07dd99b5b43d
olehkhalin <olehkhalin@gmail.com>
2021-03-13 18:57:51 +0200

    Set up SASS & classnames + created _variables & _breakpoints

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,7 @@
-import React from "react";
-import { AppProps } from "next/app";
-import '../styles/globals.css';
+import React from 'react';
+import { AppProps } from 'next/app';
+
+import '@styles/globals.sass';
 
 function MyApp({ Component, pageProps }: AppProps) {
   return <Component {...pageProps} />;
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,42 +1,44 @@
 import React from 'react';
+import cx from 'classnames';
 import Head from 'next/head';
-import styles from '../styles/Home.module.css';
+
+import s from '@styles/Home.module.sass';
 
 export default function Home() {
   return (
-    <div className={styles.container}>
+    <div className={s.container}>
       <Head>
         <title>Create Next App</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
-      <main className={styles.main}>
-        <h1 className={styles.title}>
+      <main className={s.main}>
+        <h1 className={s.title}>
           Welcome to
           {' '}
           <a href="https://nextjs.org">Next.js!</a>
         </h1>
 
-        <p className={styles.description}>
+        <p className={s.description}>
           Get started by editing
           {' '}
-          <code className={styles.code}>pages/index.js</code>
+          <code className={s.code}>pages/index.tsx</code>
         </p>
 
-        <div className={styles.grid}>
-          <a href="https://nextjs.org/docs" className={styles.card}>
+        <div className={s.grid}>
+          <a href="https://nextjs.org/docs" className={cx(s.card, s.additionalCard)}>
             <h3>Documentation &rarr;</h3>
             <p>Find in-depth information about Next.js features and API.</p>
           </a>
 
-          <a href="https://nextjs.org/learn" className={styles.card}>
+          <a href="https://nextjs.org/learn" className={s.card}>
             <h3>Learn &rarr;</h3>
             <p>Learn about Next.js in an interactive course with quizzes!</p>
           </a>
 
           <a
             href="https://github.com/vercel/next.js/tree/master/examples"
-            className={styles.card}
+            className={s.card}
           >
             <h3>Examples &rarr;</h3>
             <p>Discover and deploy boilerplate example Next.js projects.</p>
@@ -44,7 +46,7 @@ export default function Home() {
 
           <a
             href="https://vercel.com/new?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className={styles.card}
+            className={s.card}
           >
             <h3>Deploy &rarr;</h3>
             <p>
@@ -54,7 +56,7 @@ export default function Home() {
         </div>
       </main>
 
-      <footer className={styles.footer}>
+      <footer className={s.footer}>
         <a
           href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
           target="_blank"
@@ -62,7 +64,7 @@ export default function Home() {
         >
           Powered by
           {' '}
-          <img src="/vercel.svg" alt="Vercel Logo" className={styles.logo} />
+          <img src="/vercel.svg" alt="Vercel Logo" className={s.logo} />
         </a>
       </footer>
     </div>
