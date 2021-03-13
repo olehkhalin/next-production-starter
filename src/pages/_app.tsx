c320dc12fef9f39fbabb74497e86b5f5d262d92e
olehkhalin <olehkhalin@gmail.com>
2021-03-13 20:45:45 +0200

    Set up Internationalization

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import { AppProps } from 'next/app';
+import { appWithTranslation } from 'next-i18next';
 
 import '@styles/globals.sass';
 
@@ -7,4 +8,4 @@ function MyApp({ Component, pageProps }: AppProps) {
   return <Component {...pageProps} />;
 }
 
-export default MyApp;
+export default appWithTranslation(MyApp);
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,62 +1,96 @@
 import React from 'react';
 import cx from 'classnames';
 import Head from 'next/head';
+import Link from 'next/link';
+import { useRouter } from 'next/router';
+import { useTranslation } from 'next-i18next';
+import { serverSideTranslations } from 'next-i18next/serverSideTranslations';
 
 import LikeIcon from '@icons/Like.svg';
 
 import s from '@styles/Home.module.sass';
 
-export default function Home() {
+const Home: React.FC = () => {
+  const router = useRouter();
+  const { t } = useTranslation(['common', 'home']);
+
   return (
     <div className={s.container}>
       <Head>
-        <title>Create Next App</title>
+        <title>{t('home:Create Next App')}</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
       <main className={s.main}>
         <h1 className={s.title}>
           <LikeIcon className={s.icon} />
-          Welcome to
+          {t('home:Welcome to')}
           {' '}
           <a href="https://nextjs.org">Next.js!</a>
         </h1>
 
         <p className={s.description}>
-          Get started by editing
+          {t('home:Get started by editing')}
           {' '}
           <code className={s.code}>pages/index.tsx</code>
         </p>
 
         <div className={s.grid}>
           <a href="https://nextjs.org/docs" className={cx(s.card, s.additionalCard)}>
-            <h3>Documentation &rarr;</h3>
-            <p>Find in-depth information about Next.js features and API.</p>
+            <h3>
+              {t('home:Documentation')}
+              {' '}
+              &rarr;
+            </h3>
+            <p>{t('home:Find in-depth information about Next.js features and API.')}</p>
           </a>
 
           <a href="https://nextjs.org/learn" className={s.card}>
-            <h3>Learn &rarr;</h3>
-            <p>Learn about Next.js in an interactive course with quizzes!</p>
+            <h3>
+              {t('home:Learn')}
+              {' '}
+              &rarr;
+            </h3>
+            <p>{t('home:Learn about Next.js in an interactive course with quizzes!')}</p>
           </a>
 
           <a
             href="https://github.com/vercel/next.js/tree/master/examples"
             className={s.card}
           >
-            <h3>Examples &rarr;</h3>
-            <p>Discover and deploy boilerplate example Next.js projects.</p>
+            <h3>
+              {t('home:Examples')}
+              {' '}
+              &rarr;
+            </h3>
+            <p>{t('home:Discover and deploy boilerplate example Next.js projects.')}</p>
           </a>
 
           <a
             href="https://vercel.com/new?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
             className={s.card}
           >
-            <h3>Deploy &rarr;</h3>
+            <h3>
+              {t('home:Deploy')}
+              {' '}
+              &rarr;
+            </h3>
             <p>
-              Instantly deploy your Next.js site to a public URL with Vercel.
+              {t('home:Instantly deploy your Next.js site to a public URL with Vercel.')}
             </p>
           </a>
         </div>
+        <span>
+          {t('common:Change language')}
+          :
+          {' '}
+          {router.locales?.map((locale) => (
+            <React.Fragment key={locale}>
+              <Link href="/" locale={locale}>{locale.toUpperCase()}</Link>
+              {' '}
+            </React.Fragment>
+          ))}
+        </span>
       </main>
 
       <footer className={s.footer}>
@@ -65,11 +99,19 @@ export default function Home() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Powered by
+          {t('common:Powered by')}
           {' '}
           <img src="/vercel.svg" alt="Vercel Logo" className={s.logo} />
         </a>
       </footer>
     </div>
   );
-}
+};
+
+export const getStaticProps = async ({ locale }: { locale: string }) => ({
+  props: {
+    ...await serverSideTranslations(locale, ['common', 'home']),
+  },
+});
+
+export default Home;
